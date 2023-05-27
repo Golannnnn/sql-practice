81a4c38091d26f0baef7c1e3d71fb9d9a9f00f2b
Golannnnn <forburacc@protonmail.ch>
2023-05-27 14:58:12 +0300

    added crud functions

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const db = require("./queries");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
